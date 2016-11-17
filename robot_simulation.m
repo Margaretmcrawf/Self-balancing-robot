c8f4ea9691432782d5f93d5a67a6f53be7a66d4c
Kevin Zhang <kzhang@ivani.com>
2016-11-17 12:01:03 -0500

    added more stuff to simulation, now set up, needs content

--- robot_simulation.m
+++ robot_simulation.m
@@ -1,18 +1,92 @@
+%uncontrolled self balancing robot simulation.
+
+
 function robot_simulation()
-%uncontrolled self balancing robot.
 
-    function res = derivs(~, vals)
-        x = vals(1);
-        theta = vals(2);
-        vx = vals(3);
-        vtheta = vals(4);
+    
+%initial variables
 
-        res = vals; %this is just plain wrong.
-    end
+%%%%% the starting constants and other unknown quantities go here
+
+
+% options = odeset('Events',@events);
+
+%initial conditions
+close all;
+
+%%%%% all starting state variables go here
+
+
+%%%%% also vector of starting conditions also go in here
+
+
+%ODE computation
+[T,U] = ode45(@balance, [0:.01:10], []);
+
+
+%%%%%% Visualizations go here
+
+% comet3(U);
+% grid on;
+title('Self Balancing Robot');
+xlabel('X (m)');
+ylabel('Y (m)');
+
+figure
+plot(T, X);
+title('X pos');
+xlabel('time (s)');
+ylabel('X (m)');
+figure
+plot(T, Y);
+title('Y pos');
+xlabel('time (s)');
+ylabel('Y (m)');
+figure
+plot(T, Z);
+title('Z pos');
+xlabel('time (s)');
+ylabel('Z (m)');
 
 
 vals0 = [0,0,0,0];
 [T, V] = ode45(@derivs, [0,10], vals0);
+
+%functions
+function res = balance(t, vals)
+
+    x = vals(1);
+    theta = vals(2);
+    vx = vals(3);
+    vtheta = vals(4);
+    
+    dTdt = vtheta;
+    dXdt = vx;
+  
+    dV = acceleration(x, theta, vx, vtheta);
+    res = [dTdt; dXdt;dV(1);dV(2)];
 end
+
+function res = acceleration(x, theta,vx, vtheta)
+
+    %%%%%  calculations for accelerations and other variables go here
+    Taccel = 0;
+    Xaccel = 0;
+    res = [Xaccel; Taccel];
+    
+end
+
+%%%% events function in case we want to limit how far the platform can
+%%%% rotate
+
+%  function [value, isterminal, direction] = events(t,y)
+%         isterminal = 1;
+%         direction = -1;
+%         value = y(2);
+%  end
+
+end
+
+
 
 
