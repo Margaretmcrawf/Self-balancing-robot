d31f35db8f770fe81e2389fdcb47aba23b7915b8
margaretmcrawf <margaretmcrawf@gmail.com>
2016-11-30 21:48:53 -0500

    uncontrolled self balancing robot sim is working

--- robot_simulation.m
+++ robot_simulation.m
@@ -23,14 +23,15 @@ r2 = .04; %outer radius of wheel
 l = .1;
 I_wheel = m_wheel/2 * (r1^2 + r2^2);
 I_plat = m_plat*l^2/12;
-Mmotor = 0;
-
+Mmotor = -1;
 
 %%%%% also vector of starting conditions also go in here
 
 %ODE computation
-vals0 = [0,0,0,0];
-[T, U] = ode45(@balance, [0,10], vals0);
+vals0 = [0, 2*pi/3,0,0];
+
+options = odeset('Events', @events);
+[T, U] = ode23s(@balance, [0,2], vals0, options);
 X = U(:,1);
 Theta = U(:,2);
 Xplat = cos(Theta);
@@ -40,29 +41,32 @@ Yplat = sin(Theta);
 
 % comet3(U);
 % grid on;
-COMX = (l/2 + r2)*cos(Theta);
-COMY = (l/2 + r2)*sin(Theta);
+COMX = (l/2)*cos(Theta);
+COMY = (l/2)*sin(Theta);
 for i=1:length(Theta)
     cla
-    line([COMX(i) -l/2*cos(Theta(i)), COMX(i) + l/2*cos(Theta(i))], [COMY(i) -l/2*sin(Theta(i)), COMY(i) + l/2*sin(Theta(i))]);
-    axis([-2 2 -2 2]);
+    hold on
+    plot(X(i) + COMX(i), COMY(i), '*');
+    line([X(i) + COMX(i) - l/2*cos(Theta(i)), X(i) + COMX(i) + l/2*cos(Theta(i))], [COMY(i) - l/2*sin(Theta(i)), COMY(i) + l/2*sin(Theta(i))]);
+    axis([-.5 .5 -.5 .5]);
     drawnow;
+    pause(.1);
 end
 title('Self Balancing Robot');
 xlabel('X (m)');
 ylabel('Y (m)');
 
-figure
-plot(T, Xplat);
-title('X pos');
-xlabel('time (s)');
-ylabel('X (m)');
-
-figure
-plot(T, Yplat);
-title('Y pos');
-xlabel('time (s)');
-ylabel('Y (m)');
+% figure
+% plot(T, Xplat);
+% title('X pos');
+% xlabel('time (s)');
+% ylabel('X (m)');
+% 
+% figure
+% plot(T, Yplat);
+% title('Y pos');
+% xlabel('time (s)');
+% ylabel('Y (m)');
 
 
 
@@ -75,20 +79,18 @@ function res = balance(~, vals)
     vtheta = vals(4);
     
     %equations matrix
-    %   x'', theta'', Fpx, Fpy, N, Ff
-    A = [m_plat, -m_plat*l*sin(theta), 1, 0, 0, 0;...
-        0, m_plat*l*cos(theta), 0, 1, 0, 0;...
-        0, I_plat, sin(theta), -cos(theta), 0, 0;...
-        -m_wheel, 0, 1, 0, 0, 1;...
-        0, 0, 0, 1, 1, 0;...
-        I_wheel/r2, 0, 0, 0, 0, r2];
+    %   x'', theta'', Fpx, Fpy, Ff
+    A = [m_plat, -m_plat*l*sin(theta), 1, 0, 0;...
+        0, m_plat*l*cos(theta), 0, 1, 0;...
+        0, -I_plat, -l/2 *sin(theta), l/2 * cos(theta), 0;...
+        m_wheel, 0, -1, 0, -1;...
+        I_wheel/r2, 0, 0, 0, r2];
     
     B = [m_plat*l*vtheta^2*cos(theta);...
         -m_plat*g + m_plat*l*vtheta^2*sin(theta);...
-        -Mmotor;...
+        Mmotor;...
         0;...
-        m_wheel*g;...
-        Mmotor];
+        -Mmotor];
     
     Xvals = A^(-1)*B;
     dvx = Xvals(1);
@@ -99,11 +101,14 @@ end
 %%%% events function in case we want to limit how far the platform can
 %%%% rotate
 
-%  function [value, isterminal, direction] = events(t,y)
-%         isterminal = 1;
-%         direction = -1;
-%         value = y(2);
-%  end
+ function [value, isterminal, direction] = events(t, y)
+        x = y(1);
+        theta = y(2);
+        ypos = (l)*sin(theta);
+        isterminal = 1;
+        direction = -1;
+        value = ypos + r2;
+ end
 
 end
 
